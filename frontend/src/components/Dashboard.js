799099d818650a461b52502fab90bf2736e92070
Lupyana <lupyanambembati@gmail.com>
2019-05-18 13:37:37 +0300

    Feat: Offer a ride
    - Creared give a ride form template
    - When user clicks give a ride button form will apear and can enter the ride details
    - Form will disapear when user presses either submit or close button
    - The give a ride button will only appear in the main(dashboard) screen
    
    see #15

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,22 +1,20 @@
 import React, { Component } from "react";
 import Header from "./partials/Header"
 import Offer from "./partials/Offer"
+import GiveARide from "./partials/GiveARide";
 import { NavLink } from 'react-router-dom';
-import '../css/form.css';
 
 class Dashboard extends Component {
   render() {
     return (
       <div>
         <Header />
-
-          <div class="width-80p center ">
-            <div class="offer-list">
-              <NavLink exact to="view-ride"><Offer /></NavLink>
-            </div>
+        <div class="width-80p center ">
+          <div class="offer-list">
+            <NavLink exact to="view-ride"><Offer /></NavLink>
           </div>
-          <button class="open-button" >Give a Ride</button>
-
+        </div>
+        <GiveARide />
       </div>
     );
   }
