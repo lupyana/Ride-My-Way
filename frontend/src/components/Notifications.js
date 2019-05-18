a0a763833a29617834d3c3c1ec895d527d339b0b
Lupyana <lupyanambembati@gmail.com>
2019-05-18 14:05:25 +0300

    Feat: View notifications

---
+++ frontend/src/components/Notifications.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import Header from "./partials/Header"
+
+class Notifications extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <div class="width-80p center ">
+          <h2>Requested Rides:</h2>
+
+          <table class="ride-list">
+            <tr>
+              <th> Date </th>
+              <th> Driver </th>
+              <th> Passenger </th>
+              <th> From </th>
+              <th> To </th>
+              <th> Fare</th>
+              <th>Status</th>
+
+            </tr>
+            <tr>
+              <td> dd/mm/yyy </td>
+              <td> Kevin </td>
+              <td> Me </td>
+              <td> Masaki </td>
+              <td> Morocco </td>
+              <td> 2,000 </td>
+              <th> Pending</th>
+            </tr>
+          </table>
+
+          <h2>Offerd Rides:</h2>
+          <p> You do not have any active offers</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Notifications;
