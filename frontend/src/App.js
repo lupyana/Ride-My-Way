c1b267c9c9734d8c10429a50d3c5cfa9182d6f34
Lupyana <lupyanambembati@gmail.com>
2019-05-18 12:41:36 +0300

    Added link to main page (dashboard)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom'
-import Login from "./components/Login";
-import Register from "./components/Register";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import Forgot from "./components/Auth/Forgot";
+import Dashboard from "./components/Dashboard";
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path='/' component={Login}></Route>
+        <Route exact path='/' component={Login}>
+        </Route>
         <Route exact path='/register' component={Register} />
+        <Route exact path='/forgot-password' component={Forgot} />
+        <Route exact path='/dashboard' component={Dashboard} />
+
       </Switch>
     </div>
   );
---
+++ frontend/src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+// import { NavLink } from 'react-router-dom';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        Dashboard
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
