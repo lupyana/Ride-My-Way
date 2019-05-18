361e1d5b2b49c2409d73ffa023bd35807161590b
Lupyana <lupyanambembati@gmail.com>
2019-05-18 13:04:02 +0300

    Enhancement: Converted offer list into its own component
    Now a single offer is its own reusalble component
    
    see  #7

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Header from "./partials/Header"
+import Offer from "./partials/Offer"
+
 import '../css/form.css';
 // import { NavLink } from 'react-router-dom';
 
@@ -11,110 +13,10 @@ class Dashboard extends Component {
 
           <div class="width-80p center ">
             <div class="offer-list">
-              <a href="ride.html">
-              <div class="offer">
-                <div class="padding-30">
-                    <div class="mb-20">
-                        <img src="https://via.placeholder.com/320x80" alt="" />
-                    </div>
-                    <div class="ride-details ">
-                      <div class="width-50 ride-start mb-10">
-                        Kkoo to Mbezi
-                      </div>
-                      <div class="width-50 ride-time text-right mb-10">
-                        Time: 1800 hrs
-                      </div>
-                    </div>
-                    <div class="ride-offerer">
-                      <div class="width-50 ride-start mb-10">
-                        <span class="avatar">With: </span> Benito
-                      </div>
-                      <div class="width-50 ride-time text-right mb-10">
-                        Rating: some stars
-                      </div>
-                    </div>
-                </div>
-              </div>
-              </a>
-              <a href="ride.html">
-              <div class="offer">
-                <div class="padding-30">
-                    <div class="mb-20">
-                        <img src="https://via.placeholder.com/320x80" alt="" />
-                    </div>
-                    <div class="ride-details ">
-                      <div class="width-50 ride-start mb-10">
-                        Kkoo to Mbezi
-                      </div>
-                      <div class="width-50 ride-time text-right mb-10">
-                        Time: 1800 hrs
-                      </div>
-                    </div>
-                    <div class="ride-offerer">
-                      <div class="width-50 ride-start mb-10">
-                        <span class="avatar">With: </span> Benito
-                      </div>
-                      <div class="width-50 ride-time text-right mb-10">
-                        Rating: some stars
-                      </div>
-                    </div>
-                </div>
-              </div>
-            </a>
-            <a href="ride.html">
-              <div class="offer">
-                <div class="padding-30">
-                    <div class="mb-20">
-                        <img src="https://via.placeholder.com/320x80" alt="" />
-                    </div>
-                    <div class="ride-details ">
-                      <div class="width-50 ride-start mb-10">
-                        Kkoo to Mbezi
-                      </div>
-                      <div class="width-50 ride-time text-right mb-10">
-                        Time: 1800 hrs
-                      </div>
-                    </div>
-                    <div class="ride-offerer">
-                      <div class="width-50 ride-start mb-10">
-                        <span class="avatar">With: </span> Benito
-                      </div>
-                      <div class="width-50 ride-time text-right mb-10">
-                        Rating: some stars
-                      </div>
-                    </div>
-                </div>
-              </div>
-            </a>
-            <a href="ride.html">
-              <div class="offer">
-                <div class="padding-30">
-                    <div class="mb-20">
-                        <img src="https://via.placeholder.com/320x80" alt="" />
-                    </div>
-                    <div class="ride-details ">
-                      <div class="width-50 ride-start mb-10">
-                        Kkoo to Mbezi
-                      </div>
-                      <div class="width-50 ride-time text-right mb-10">
-                        Time: 1800 hrs
-                      </div>
-                    </div>
-                    <div class="ride-offerer">
-                      <div class="width-50 ride-start mb-10">
-                        <span class="avatar">With: </span> Benito
-                      </div>
-                      <div class="width-50 ride-time text-right mb-10">
-                        Rating: some stars
-                      </div>
-                    </div>
-                </div>
-              </div>
-            </a>
-
+              <Offer />
             </div>
           </div>
-          <button class="open-button" onclick="openForm()">Give a Ride</button>
+          <button class="open-button" >Give a Ride</button>
 
       </div>
     );
---
+++ frontend/src/components/partials/Offer.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { NavLink } from 'react-router-dom';
+
+class Offer extends Component {
+  render () {
+    return (
+      <div class="offer">
+        <div class="padding-30">
+            <div class="mb-20">
+                <img src="https://via.placeholder.com/320x80" alt="" />
+            </div>
+            <div class="ride-details ">
+              <div class="width-50 ride-start mb-10">
+                Kkoo to Mbezi
+              </div>
+              <div class="width-50 ride-time text-right mb-10">
+                Time: 1800 hrs
+              </div>
+            </div>
+            <div class="ride-offerer">
+              <div class="width-50 ride-start mb-10">
+                <span class="avatar">With: </span> Benito
+              </div>
+              <div class="width-50 ride-time text-right mb-10">
+                Rating: some stars
+              </div>
+            </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Offer;
