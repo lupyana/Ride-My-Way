fd0abb5c8a3dd10e3af5808dc166bcf0684ea307
Lupyana <lupyanambembati@gmail.com>
2019-05-24 15:11:04 +0300

    Chore: Added jest extended matchers

--- backend/tests/index.test.js
+++ backend/tests/index.test.js
@@ -5,7 +5,6 @@ describe('Test the root path', () => {
   test('It should response the GET method', () => request(app)
     .get('/api/v1')
     .then((response) => {
-      console.log(response);
       expect(response).toBeDefined();
       expect(response.statusCode).toBe(200);
       expect(response.body.message).toBe('Hello world');
