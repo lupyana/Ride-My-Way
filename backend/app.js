53e374d791ab08b71a7bfc1628005f6476120f79
Lupyana <lupyanambembati@gmail.com>
2019-05-25 16:51:01 +0300

    Fix: Remove redundant import(body-parser)

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const router = require('./router/routes');
 
 const app = express();
 const port = 3000;
 
 app.use('/api/v1', router);
-
-// parse various different custom JSON types as JSON
-app.use(bodyParser.json({ type: 'application/*+json' }));
 
 module.exports = app;
 
